00843db77b13b1b78ae16ab53ac860003d8ad7cd
Deborah McVey <deborah.mcvey@gmail.com>
2024-02-03 14:00:15 -0500

    events and users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_03_173838) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_03_183709) do
   create_table "comments", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "commentable_type", null: false
@@ -20,6 +20,27 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_03_173838) do
     t.datetime "updated_at", null: false
     t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "event_participants", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "event_id", null: false
+    t.integer "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_event_participants_on_event_id"
+    t.index ["user_id"], name: "index_event_participants_on_user_id"
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.text "content"
+    t.datetime "start_date_time"
+    t.datetime "end_date_time"
+    t.integer "guests"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "locations", force: :cascade do |t|
@@ -61,6 +82,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_03_173838) do
   end
 
   add_foreign_key "comments", "users"
+  add_foreign_key "event_participants", "events"
+  add_foreign_key "event_participants", "users"
+  add_foreign_key "events", "users"
   add_foreign_key "posts", "users"
   add_foreign_key "profiles", "users"
 end
