3ecb470f0ac06a78cbc92f214c9c8d5b251129c6
Deborah McVey <deborah.mcvey@gmail.com>
2024-02-05 21:40:29 -0500

    ready for resources :posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,22 @@
 Rails.application.routes.draw do
  # localhost:3000/users
- get '/users', to: 'users#index'
+ #get '/users', to: 'users#index'
+
+ # localhost:3000/users/1
+ #get '/users/:id', to: 'users#show'
+
+ # localhost:3000/users
+ #post '/users', to: 'users#create'
+
+ # localhost:3000/users/1
+ #put '/users/:id', to: 'users#update'
+
+ # localhost:3000/users/1
+ #put '/users/:id', to: 'users#destroy'
+
+ resources :users do
+    get 'posts', to: 'users#posts_id'
+ end
+
+ resources :posts
 end
