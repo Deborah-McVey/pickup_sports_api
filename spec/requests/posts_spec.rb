751cbe3f57ca59fb44f78341cac0ceb5b1d1c1c1
Deborah McVey <deborah.mcvey@gmail.com>
2024-02-12 20:46:11 -0500

    beginning rspec posts

---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe "Posts", type: :request do
+  describe "GET /posts" do
+
+    let(:post) {create(:post)}
+
+    before do
+      # creating the post
+      post
+      get "/posts"
+    end
+
+    # returns a successful response
+    it "returns a successfule response" do
+      expect(response).to be_successful
+    end
+    # returns a response with all posts
+    it "returns a response with all posts" do
+      expect(response.body).to eq(Post.all.to_json)
+    end
+  end
+
+  # show
+
+  # create
+
+  # update
+
+  # destoy
+  
+end
