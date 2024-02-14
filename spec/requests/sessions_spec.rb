1cacbe175b108691bfaf2673f972f59fc0f461ad
Deborah McVey <deborah.mcvey@gmail.com>
2024-02-14 09:32:51 -0500

    spec/requests/sessions spec

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "Sessions", type: :request do
-  describe "GET /create" do
-    it "returns http success" do
-      get "/sessions/create"
+  describe "POST /login" do
+    let(:user) { create(:user) }
+
+    it 'authenticate the user and returns a success response' do
+      post "/login", params { user_name}
       expect(response).to have_http_status(:success)
     end
   end
