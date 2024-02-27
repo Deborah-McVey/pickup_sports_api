c4c8e8e00f8417ab75494f36657a1833f476a012
Deborah McVey <deborah.mcvey@gmail.com>
2024-02-26 21:04:51 -0500

    need token and header in every request spec, see authenticating user requests video

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,9 +2,24 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "GET /users" do
-    it "works! (now write some real specs)" do
-      get users_index_path
-      expect(response).to have_http_status(200)
+    let(:user) {create(:user)}
+    let(:token) { auth_token_for_user(:user) }
+
+    before do
+      # creating the user
+      user
+      get "/users/#{user.id}", 
+      headers: { Authorization: "Bearer #{token}" }
+    end
+
+    # returns a successful response
+    it "returns a successful response" do
+      expect(response).to be_successful
+    end
+
+    # return a response with all the users
+    it "returns a response with all the users" do
+      expect(response.body).to eq(User.all.to_json)
     end
   end
 end
--- spec/support/auth_helpers.rb
+++ spec/support/auth_helpers.rb
@@ -1,5 +1,5 @@
 module AuthHelpers
     def auth_token_for_user(user)
-        JWT.encode({user_id: user_id}, Rails.application.secret_key_base)
+        JWT.encode({user_id: user.id}, Rails.application.secret_key_base)
     end
 end
