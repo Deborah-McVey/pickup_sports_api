d54ff2d32eeef5b2c68b2f243197b6056ed6e332
Deborah McVey <deborah.mcvey@gmail.com>
2024-02-27 21:10:30 -0500

    change from video Building Login Functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,5 @@
 Rails.application.routes.draw do
-  get 'profiles/show'
-  get 'sessions/create'
- # localhost:3000/users
- #get '/users', to: 'users#index'
-
- # localhost:3000/users/1
- #get '/users/:id', to: 'users#show'
-
- # localhost:3000/users
- #post '/users', to: 'users#create'
-
- # localhost:3000/users/1
- #put '/users/:id', to: 'users#update'
-
- # localhost:3000/users/1
- #put '/users/:id', to: 'users#destroy'
-
- scope '/' do
+  scope '/' do
    post 'login', to: 'sessions#create'
  end
 
